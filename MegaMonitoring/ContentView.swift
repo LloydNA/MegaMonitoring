8f4732399fa44a2d3c4e9406d24ee32b8cc04a8a
lloydnas <madry0404@yahoo.com>
2023-06-10 09:03:47 -0600

    Initial Commit

---
+++ MegaMonitoring/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MegaMonitoring
+//
+//  Created by Nestor Adrian Sandoval Ortiz on 10/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MegaMonitoring/MegaMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaMonitoringApp.swift
+//  MegaMonitoring
+//
+//  Created by Nestor Adrian Sandoval Ortiz on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MegaMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
